ec18a1eda106e1618cbb262f2ad8d6419860b590
Alison <alisonsvargas@gmail.com>
2020-09-04 18:25:50 -0300

    Feat: View para tags

---
+++ src/Controller/TagController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\{Product, Tag, Image};
+
+class TagController extends AbstractController
+{
+    /**
+     * @Route("/tag", name="tag")
+     */
+    public function index()
+    {
+        $tags = $this->getDoctrine()->getRepository(Tag::class)->findAll();
+        return $this->render('tag/index.html.twig', [
+            'controller_name' => 'TagController',
+            'tags' => $tags
+        ]);
+    }
+}
