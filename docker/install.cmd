545c6362cdd40fdabc2017c7ae6cf96e009a302e
Alison <alisonsvargas@gmail.com>
2020-09-06 00:19:43 -0300

    Feat:Dockerizado + Script de instalação multi plataforma.

---
+++ docker/install.cmd
@@ -0,0 +1,22 @@
+echo off
+
+goto(){
+# Linux code here
+docker-compose build
+docker-compose up -d
+docker-compose exec php php bin/console doctrine:migrations:migrate -n
+docker-compose exec php php bin/console doctrine:fixtures:load -n
+uname -o
+}
+
+goto $@
+exit
+
+:(){
+rem Windows script here
+docker-compose build
+docker-compose up -d
+docker-compose exec php php bin/console doctrine:migrations:migrate -n
+docker-compose exec php php bin/console doctrine:fixtures:load -n
+echo %OS%
+exit
